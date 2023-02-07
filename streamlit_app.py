c6832edac951c6e4c7c399a397eaf860a3652870
america-says-data <america.says.data@gmail.com>
2023-02-07 18:37:47 -0500

    added google sheet plugin

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,20 @@ st.write ("""
 Hello *world!*
 """)
 
+sheet_id = "1wecLQmlElnGaUP92uVEgT0bdyqqwt4HTVlTaqyFFCIw"
+sheet_name = "Question"
+url_1 = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+sheet_name = "Game"
+url_2 = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+sheet_name = "Team"
+url_3 = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+
+df_question = pd.read_csv(url_1)
+df_game = pd.read_csv(url_2)
+df_team = pd.read_csv(url_3)
+
+
+print(df_question.head())
+
