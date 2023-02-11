3cf0cf956766a4444d3a4177871a7cb1296550a1
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-02-10 22:11:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ def build_players_table():
 			from df_question_tally 
 			group by SEASON, GAME, TEAM, ROUND, VALUE
 			""")
-	df_question_tally_new_check = df_question_tally.groupby(["Season","Game","Team","Round","value"]).count().reset_index(name="NUM_ANSWERS")
+	df_question_tally_new_check = df_question_tally.groupby(["Season","Game","Team","Round","value"]).count().reset_index()
 	
 	print(df_question_tally_new.head())
 	print(df_question_tally_new_check.head())
