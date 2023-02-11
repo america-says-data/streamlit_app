77ace603ae32a2d7132599bc52e22a6b5abf5c88
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-02-11 00:22:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
 
-gc = gspread.Client(credentials=credentials)
+gc = gspread.auth.authorize(credentials=credentials)
 
 sheet = gc.open_by_key("1wecLQmlElnGaUP92uVEgT0bdyqqwt4HTVlTaqyFFCIw")
 
