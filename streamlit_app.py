fc958812c5d8d7e69eb2ab47854b4f6ca49de866
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-02-12 11:29:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ gc = gspread.service_account(filename='.config/gspread/service_account.json')
 
 sheet = gc.open_by_key("1wecLQmlElnGaUP92uVEgT0bdyqqwt4HTVlTaqyFFCIw")
 
-@st.cache_data
+@st.cache_data(ttl=36000)
 def get_tables():
 	df_question = pd.DataFrame(sheet.worksheet("Question").get_all_records())
 	df_game = pd.DataFrame(sheet.worksheet("Game").get_all_records())
@@ -44,7 +44,7 @@ st.write("Last update - February 10th, 2023")
 
 
 #### TODO : Create streamlit loading text that says "Creating Player Table"
-@st.cache_data
+@st.cache_data(ttl=36000)
 def build_players_table():
 	df_individual = df_team[["Season", "Game", "Game_id", "Team", "Team_Num", "Team_id", "Captain", "Member_2", "Member_3", "Member_4"]]
 	df_individual = pd.melt(df_individual, 
@@ -111,7 +111,7 @@ def build_players_table():
 
 df_players = build_players_table()
 
-@st.cache_data
+@st.cache_data(ttl=36000)
 def best_question():
 	return ps.sqldf("""
         select distinct q.SEASON, q.GAME, q.ROUND, t.TEAM, TIME_REMAINING, QUESTION_TEXT
@@ -124,7 +124,7 @@ def best_question():
         order by TIME_REMAINING desc
         """)
 
-@st.cache_data
+@st.cache_data(ttl=36000)
 def best_bonus_round():
 	return ps.sqldf("""
                 select SEASON, GAME, WINNER, AFTER_SKIPPED_TIME_REMAINING, BONUS_Q_1, BONUS_Q_2, BONUS_Q_3, BONUS_Q_4
@@ -133,7 +133,7 @@ def best_bonus_round():
                 order by AFTER_SKIPPED_TIME_REMAINING desc
                 """)
 
-@st.cache_data
+@st.cache_data(ttl=36000)
 def worst_question():
 	return ps.sqldf("""
                 select distinct q.SEASON, q.GAME, q.ROUND, t.TEAM, ANSWERS_CORRECT_BY_ANSWERING_TEAM, QUESTION_TEXT
@@ -146,7 +146,7 @@ def worst_question():
                 order by ANSWERS_CORRECT_BY_ANSWERING_TEAM
                 """)
 
-@st.cache_data
+@st.cache_data(ttl=36000)
 def top_player_of_team():
 	return ps.sqldf("""
                 select p.PLAYER, p.TEAM, p.TOTAL_ANSWERS_CORRECT, t.TOTAL_ANSWERS
@@ -160,7 +160,7 @@ def top_player_of_team():
 		limit 11
                 """)
 
-@st.cache_data
+@st.cache_data(ttl=36000)
 def top_player_overall():
 	return ps.sqldf("""
                         select p.PLAYER, p.ANSWERS_CORRECT_NO_BONUS, p.TOTAL_ANSWERS_CORRECT
