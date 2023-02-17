cf512603b702a670963d0bf8e8af011cd4b14e64
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-02-17 18:05:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,7 +205,7 @@ st.bar_chart(df_dist[["Percent Times that Number of Answers is Provided"]])
 st.write("Histogram of Answers Correct by Round (by answering team)")
 
 
-df_dist = ps.sqldf("""select ROUND, ANSWERS_CORRECT_BY_ANSWERING_TEAM, 
+df_dist_round = ps.sqldf("""select ROUND, ANSWERS_CORRECT_BY_ANSWERING_TEAM, 
 		100.00*COUNT(*) / (select count(*) from df_question where QUESTION_TEXT <> 'NA' and QUESTION_TEXT is not null AND QUESTION_TEXT <> '') as 'Percent Times that Number of Answers is Provided'
                 from df_question
 		where QUESTION_TEXT <> 'NA' and QUESTION_TEXT is not null AND QUESTION_TEXT <> ''
@@ -213,10 +213,15 @@ df_dist = ps.sqldf("""select ROUND, ANSWERS_CORRECT_BY_ANSWERING_TEAM,
 		group by ROUND, ANSWERS_CORRECT_BY_ANSWERING_TEAM
                 order by ANSWERS_CORRECT_BY_ANSWERING_TEAM 
                 """.format(season_select_clause=season_select_clause))
-df_dist.reset_index(inplace=True)
-df_dist = df_dist.set_index("Answers_Correct_By_Answering_Team")
+df_dist_round.reset_index(inplace=True)
+df_dist_round = df_dist_round.set_index("Answers_Correct_By_Answering_Team")
 
-st.bar_chart(df_dist[["Round", "Percent Times that Number of Answers is Provided"]])
+df_dist_round = df_dist_round[["Round", "Percent Times that Number of Answers is Provided"]].pivot(index="Answers_Correct_By_Answering_Team"
+									     , columns="Round"
+									     , values="Percent Times that Number of Answers is Provided")
+
+st.bar_chart(df_dist_round)
+
 
 
 option = st.selectbox(
