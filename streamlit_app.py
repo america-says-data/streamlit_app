e88d73ebe3ddab759d905ada259a0ec6f48988b9
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-02-17 18:48:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -216,8 +216,9 @@ df_dist_round = ps.sqldf("""select ROUND, ANSWERS_CORRECT_BY_ANSWERING_TEAM,
                 """.format(season_select_clause=season_select_clause))
 
 df_dist_round = df_dist_round[["Answers_Correct_By_Answering_Team", "Round", "Percent Times that Number of Answers is Provided"]].pivot(
-										index = "Answers_Correct_By_Answering_Team"
-										, columns="Round"
+										#index = "Answers_Correct_By_Answering_Team"
+										#, 
+										columns="Round"
 									     	, values="Percent Times that Number of Answers is Provided"
 										)
 
