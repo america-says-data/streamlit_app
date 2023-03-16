5468d08fd5fedbefb3d17155307658ea0394e5de
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-16 11:42:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,13 @@ st.write ("""
 
  
 
-#gc = gspread.service_account(filename='.config/gspread/service_account.json')
+gc = gspread.service_account(filename='.config/gspread/service_account.json')
 
 #### TODO : figure out the versioning of oauth2 etc to handle secrets
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"]
-)
-gc = gspread.oauth_from_dict(credentials=credentials)
+# credentials = service_account.Credentials.from_service_account_info(
+#     st.secrets["gcp_service_account"]
+# )
+# gc = gspread.oauth_from_dict(credentials=credentials)
 
 sheet = gc.open_by_key("1wecLQmlElnGaUP92uVEgT0bdyqqwt4HTVlTaqyFFCIw")
 
