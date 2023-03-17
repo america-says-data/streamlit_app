818e2e4d3288f695ada11dbf07fff58d4d702b67
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-16 22:15:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -741,8 +741,8 @@ with tab3:
 	fig = px.histogram(df_team, x="Score_check", nbins=20, color_discrete_sequence=['lavender'])
 	
 	df_specific_game = df_team[df_team.Game_id == game_find][['Team', 'Score_check']]
-	team_1 = df_specific_game.loc[0]
-	team_2 = df_specific_game.loc[1]
+	team_1 = df_specific_game.iloc[0,0]
+	team_2 = df_specific_game.iloc[1,0]
 	if team_1.Score_check >= team_2.Score_check:
 		team_1['pos'] = "top right"
 		team_2['pos'] = "top left"
