a19ccedb6354abfdef4ef6979f505175a87ce45b
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-17 22:26:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -888,9 +888,9 @@ with tab3:
 	
 	fig = px.histogram(df_bonus_quick, x="After_Skipped_Time_Remaining", nbins=20, color_discrete_sequence=['lavender'])
 	fig.update_layout(title="Bonus Round Time To Fill All Boards", xaxis_title="Time Remaining on the Clock", yaxis_title="Number of Teams Successful in that Time Bucket")		
-	indicator = df_spec_game.iloc[0]['Is_winner']
-	time_remaining = df_spec_game.iloc[0]['After_Skipped_Time_Remaining']
-	winning_team = df_spec_game.iloc[0]['Winner']
+	indicator = df_spec_game['Is_winner'].get_values()[0]
+	time_remaining = df_spec_game['After_Skipped_Time_Remaining'].get_values()[0]
+	winning_team = df_spec_game['Winner'].get_values()[0]
 	
 	if not spoiler:
 		st.header("Does the winning team win the bonus round? Click Spoiler to find out or tune in!")
