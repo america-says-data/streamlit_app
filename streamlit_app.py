95d6ef5f9a61c2e99431a1a968747d3633ec7d5a
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-17 21:00:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -880,8 +880,7 @@ with tab3:
 		st.header("Does the winning team win the bonus round?")
 		
 		df_bonus_quick = df_game[df_game.After_Skipped_Time_Remaining.notna()][["Game_id", "Winner", "After_Skipped_Time_Remaining"]]
-		df_spec_game = df_bonus_quick[df_bonus_quick.Game_id == game_find_1]
-		st.dataframe(df_bonus_quick)
+		df_spec_game = df_bonus_quick[df_bonus_quick.Game_id == game_find_1].iloc[0]
 		fig = px.histogram(df_bonus_quick, x="After_Skipped_Time_Remaining", nbins=20, color_discrete_sequence=['lavender'])
 		fig.add_vline(x=df_spec_game.After_Skipped_Time_Remaining, line_dash="dot", annotation_text=df_spec_game.Winner, annotation_position="top right", line_color="blue")
 	
