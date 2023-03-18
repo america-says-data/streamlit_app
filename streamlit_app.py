3cd84ce418b9389f5b7f47fb4b261072a927841b
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-18 13:25:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -758,9 +758,14 @@ with tab3:
 	else:
 		st.write("No Game Selected")
 	
-	spoiler = st.checkbox('Spoilers - uncheck if new game selected')
+	
+	with st.form("run_game", clear_on_submit = True):
+		game_select = st.selectbox(list(game_find_1)+[""])
+		spoiler = st.checkbox('Spoilers')
+		submission = form.form_submit_button("Run this game")
 		
-
+	st.write("the spoiler: ", spoiler)
+	st.write("game select: ", game_select)
 	st.markdown("""---""")
 ##----------------------------------------------------------------------------------------------------------------------------------------------------
 ## build game histogram
