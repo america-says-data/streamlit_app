529b8df8fb4f763e7a778e4cac151c731183e7dc
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-17 21:46:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -883,7 +883,7 @@ with tab3:
 		
 	df_bonus_quick = df_game_adjusted[df_game_adjusted.After_Skipped_Time_Remaining.notna()]
 	df_spec_game = df_bonus_quick[df_bonus_quick.Game_id == game_find_1]
-	st.write(df_bonus_quick.Is_winner.values())
+	st.write(df_bonus_quick['Is_winner'])
 	fig = px.histogram(df_bonus_quick, x="After_Skipped_Time_Remaining", nbins=20, color_discrete_sequence=['lavender'])
 	fig.update_layout(title="Plot Title", xaxis_title="X Axis Title", yaxis_title="Y Axis Title")		
 	if not spoiler:
