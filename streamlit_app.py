7e5f8b1f4fb4f3ca14173e31d7de41658b6cc79b
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-17 22:10:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -883,7 +883,7 @@ with tab3:
 
 		
 	df_bonus_quick = df_game_adjusted[df_game_adjusted.After_Skipped_Time_Remaining.notna()]
-	df_spec_game = df_bonus_quick[df_bonus_quick.Game_id == game_find_1].iloc[[0]]
+	df_spec_game = df_bonus_quick[df_bonus_quick.Game_id == game_find_1]
 	
 	fig = px.histogram(df_bonus_quick, x="After_Skipped_Time_Remaining", nbins=20, color_discrete_sequence=['lavender'])
 	fig.update_layout(title="Bonus Round Time To Fill All Boards", xaxis_title="Time Remaining on the Clock", yaxis_title="Number of Teams Successful in that Time Bucket")		
