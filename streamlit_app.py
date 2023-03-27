39b84afe05bd917d3018bfc4126f54650c8602af
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-27 00:26:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1067,8 +1067,10 @@ with tab3:
 	
 	if st.session_state.spoiler:
 		game_flow_final_df = game_flow_table[game_flow_table.GAME_ID == st.session_state.game_select]
-		fig = px.line(game_flow_final_df, x = "variable", y = "value", color = "TEAM_NUM", color_discrete_map = {1:'red', 2:'blue'})
-		fig.for_each_trace(lambda t: t.update(name = game_flow_final_df[t.TEAM_NAME]))
+		flow_team_1 = game_flow_final_df[game_flow_final_df['TEAM_NUM'] == 1].iloc[0]
+		flow_team_2 = game_flow_final_df[game_flow_final_df['TEAM_NUM'] == 2].iloc[0]
+		fig = px.line(flow_team_1, x = "variable", y = "value", line_color="red")
+	#	fig.for_each_trace(lambda t: t.update(name = game_flow_final_df[t.TEAM_NAME]))
 		st.plotly_chart(fig, use_container_width=True)
 
 		
