93563f3d6db049a79040beb90659c5527abc1fcb
america-says-data <124734380+america-says-data@users.noreply.github.com>
2023-03-26 20:20:34 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -268,7 +268,7 @@ def create_probability():
 
 win_rate, win_prob = create_probability()
 
-#@st.cache_data(ttl=86400)
+@st.cache_data(ttl=86400)
 def game_flow_table():
 	game_flow_table = ps.sqldf("""
 	select * 
@@ -1067,7 +1067,7 @@ with tab3:
 	
 	if st.session_state.spoiler:
 		game_flow_final_df = game_flow_table[game_flow_table.GAME_ID == st.session_state.game_select]
-		fig = px.scatter(game_flow_final_df, x = "variable", y = "value", color = "TEAM_NAME")
+		fig = px.line(game_flow_final_df, x = "variable", y = "value", color = "TEAM_NAME")
 		st.plotly_chart(fig, use_container_width=True)
 
 		
