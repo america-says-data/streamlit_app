94828a4620ddd65a1682ba8c74be07833507acca
america-says-data <124734380+america-says-data@users.noreply.github.com>
2026-04-15 20:25:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,7 @@ st.write("Currently built off of ", len(df_game), " games")
 st.write("Last update - March 18th, 2023")
 
 if "url_game_find" not in st.session_state:
-	url_check = st.experimental_get_query_params()
+	url_check = st.query_params()
 	try:
 		st.session_state["url_game_find"] = dict(url_check)["game_id"][0]
 	except (KeyError, TypeError):
